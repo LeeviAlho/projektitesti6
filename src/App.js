25262130e7770aaa0f2f44e3b831123c1a4f432d
Leevi Alho <54808340+LeeviAlho@users.noreply.github.com>
2020-12-17 22:42:48 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col s12 title-text">
+          <p>WASC</p>
+        </div>
+        <div className="col s12 subtitle-text">
+          Web Application Social Network
+        </div>
+      </div>
+    </div>
+  );
+}
